f50722a819a111622bfac524702dbc1009d92b6d
Varez Technologies <vareztechnologiess@gmail.com>
2026-08-30 02:50:07 +0500

    Deploy latest build to gh-pages with fast splash and real CartManager

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4241893065" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1004405687" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
