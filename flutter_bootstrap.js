564837a9a170f80c58e5c46ead11c7e07cbb8d73
Varez Technologies <vareztechnologiess@gmail.com>
2026-08-30 07:51:12 +0500

    Deploy latest build with AppToast and dynamic real-time Orders system to gh-pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1741293161" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3367653227" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
